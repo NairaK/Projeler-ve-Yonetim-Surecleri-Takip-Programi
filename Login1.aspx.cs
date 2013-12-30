9600990c01c2735125f50585bbf88aece45b8b29
MertDemirdelen <mert.demirdelen91@gmail.com>
2013-12-30 02:16:13 +0200

    Update Login1.aspx.cs

--- Login1.aspx.cs
+++ Login1.aspx.cs
@@ -24,7 +24,16 @@ namespace WebApplication3
             SqlOps sql = new SqlOps();
             string query = "SELECT COUNT(*) FROM UserTable1 WHERE UserName = '" + txtUsername.Text + "' AND Password ='" + txtPassword.Text + "'"; //Querry
             int result = sql.LoginCmd(query);   //ExecuteScalar
-           
+           if (result == 1)// (result >= 1)
+            {
+                query = "SELECT Yetki FROM UserTable1 WHERE UserName = '" + txtUsername.Text + "' AND Password = '" + txtPassword.Text + "'"; //Querry
+                yetki = sql.LoginCmd(query);
+                KullaniciAdi = txtUsername.Text;
+                Response.Redirect("Yonlendir.aspx?yetki="+yetki+"");
+            }
+            else
+                lblMessage.Text = "Kullanıcı adı veya şifre yanlış!";
+        
         }
 
         public string getKAdi()
