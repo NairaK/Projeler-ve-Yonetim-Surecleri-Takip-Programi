07b61f19085074b3b7cc8da50ba7fc8928a2a553
pelinsuonal <pelinsuonal@gmail.com>
2014-01-06 13:43:13 +0200

    Sayfa yönlerdirme işlemleri yapıldı

---
+++ Yonlendir.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Yonlendir : System.Web.UI.Page
+    {
+        public string yetki;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            yetki = Request.QueryString["yetki"];
+        }
+    }
+}
